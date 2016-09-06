ae8b9d9815fdc08cd16a31d11509ae3a615f3157
johnnypestana <johnny_gabriel@outlook.com>
2016-09-06 20:30:30 -0300

    Finalizadas tasks gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,34 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const clean = require('gulp-clean');
+const livereload = require('gulp-server-livereload');
 
 gulp
-	.task('jsx', ['clean'], () =>
-		gulp.src('src/jsx/app.jsx')
-			.pipe(babel({
-				presets: ['react']
-			}))
-			.pipe(gulp.dest('dist/js'))
-	)
-	.task('move', ['clean'], () => 
-		gulp.src(['src/index.html', 'src/libs/**/*'], { base: 'src'})
-			.pipe(gulp.dest('dist'))
-	)
-	.task('clean', () => 
-		gulp.src('dist', { read: false})
-			.pipe(clean())
-	)
-	.task('default', ['move', 'jsx']);
+    .task('jsx', () =>
+        gulp.src('src/jsx/app.jsx')
+            .pipe(babel({
+                presets: ['react']
+            }))
+            .pipe(gulp.dest('dist/js'))
+    )
+    .task('move', () =>
+        gulp.src(['src/index.html', 'src/libs/**/*'], { base: 'src'})
+            .pipe(gulp.dest('dist'))
+    )
+    .task('clean', () => 
+        gulp.src('dist', { read: false})
+            .pipe(clean())
+    )
+    .task('watch', () => 
+        gulp.watch('src/**/*', ['build'])
+    )
+    .task('server', () =>
+        gulp.src('dist')
+            .pipe(livereload({
+                livereload: true,
+                defaultFile: 'index.html',
+                log: 'debug'
+            }))
+    )
+    .task('build', ['jsx', 'move'])
+    .task('default', ['watch', 'server']);
