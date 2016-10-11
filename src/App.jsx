924bd9f848d094f974f917167aa47cd35026e846
johnnypestana <johnny_gabriel@outlook.com>
2016-10-11 18:19:47 -0300

    Adicionadas rotas dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,56 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
+import { render } from 'react-dom'
 import LiveSearch from './components/LiveSearch'
 import Compra from './components/Compra'
 import Posts from './components/Posts'
 import produtos from './data'
 
-ReactDOM.render(
+const App = React.createClass({
+    render() {
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-md-offset-2 col-md-8">
+                        { this.props.children }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+})
+
+const Erro = React.createClass({
+    render() {
+        return <h2> Não encontrado! </h2>
+    }
+})
+
+const LiveSearchApp = () =>
+    <LiveSearch 
+        searchKey="nome"
+        viewKey="nome"
+        data={ produtos }
+        onSelect={ console.log } />
+
+const PostsApp = () =>
+    <Posts endpoint="http://jsonplaceholder.typicode.com/posts" />
+
+const CompraApp = () =>
     <Compra
         titulo="Compras"
         subtitulo="Selecione os produtos que deseja levar"
-        produtos={produtos} />,
-    // <Posts endpoint="http://jsonplaceholder.typicode.com/posts" />,
-    // <LiveSearch 
-    //     initialValue="ar"
-    //     searchKey="nome"
-    //     viewKey="nome"
-    //     data={produtos}
-    //     onSelect={ console.log } />,
-    document.getElementById('content')
-);
+        produtos={ produtos } />
+
+const routes = {
+    path: '/',
+    component: App,
+    childRoutes: [
+        { path: 'posts', component: PostsApp },
+        { path: 'compras', component: CompraApp },
+        { path: 'livesearch', component: LiveSearchApp },
+        { path: '*', component: Erro }
+    ]
+}
+
+render(<Router history={ browserHistory } routes={ routes } />, document.body)
